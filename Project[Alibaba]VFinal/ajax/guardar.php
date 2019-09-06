e0f71c724c4746035d9bb4f1b8c3c0b90008330b
Cristian Alexander Martinez <thealxzerox@gmail.com>
2019-09-05 21:10:40 -0600

    reseteando areas

--- Project[Alibaba]VFinal/ajax/guardar.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $archivo = fopen("../data/usuarios.json","a+");
-    fwrite($archivo, json_encode($_POST) . "\n");
-    fclose($archivo);
-    $respuesta["codigo"]= 0;
-    $respuesta["mensaje"]= "Registro guardado exitosamente";
-    echo json_encode($respuesta);
-?>
